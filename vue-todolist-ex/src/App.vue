d5ebb4ab958a008626a34772581a1674c288f620
yhs <soft4750@gmail.com>
2019-12-29 03:23:19 +0900

    Vue Todolist Ex

--- vue-todolist-ex/src/App.vue
+++ vue-todolist-ex/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
       <TodoHeader></TodoHeader>
-      <TodoInput></TodoInput>
-      <TodoList></TodoList>
-      <TodoFooter></TodoFooter>
+      <TodoInput v-on:addTodo = "addTodo"></TodoInput>
+      <TodoList v-bind:propsdata="todoItems" @removeTodo = "removeTodo"></TodoList>
+      <TodoFooter v-on:removeAll = "clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -14,6 +14,33 @@ import TodoList from './components/TodoList';
 import TodoFooter from './components/TodoFooter';
 
 export default {
+  props: ['propsdata'],
+  data(){
+    return {
+      todoItems: []
+    }
+  },
+  created(){
+      if(localStorage.length > 0) {
+          for(var i = 0; i < localStorage.length; i++) {
+              this.todoItems.push(localStorage.key(i));
+          }
+      }
+  },
+  methods: {
+    addTodo(todoItem){
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+    clearAll(){
+      localStorage.clear();
+      this.todoItems = [];
+    },
+    removeTodo(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
   components: {
     'TodoHeader' : TodoHeader,
     'TodoInput' : TodoInput,
@@ -24,4 +51,18 @@ export default {
 </script>
 
 <style>
+    body {
+        text-align: center;
+        background-color: #F6F6F8;
+    }
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+    button {
+        border-style: groove;
+    }
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+    }
 </style>
