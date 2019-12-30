0abab4a2bc9d91a1d33075fc050a2792d62be85f
yhs <soft4750@gmail.com>
2019-12-31 02:11:19 +0900

    Vue Chart Ex

---
+++ vue-chart-ex/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <button type = "button" class = "btn btn-primary btn-lg">Large Button</button>
+    <Chart></Chart>
+  </div>
+</template>
+
+<script>
+import Chart from './components/Chart.vue'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      msg : 'Welcome to Your Vue.js App'
+    }
+  },
+  components: {
+    Chart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-chart-ex/src/components/Chart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id = "container" style = "width:100%;height:400px;"></div>
+</template>
+
+<script>
+import Highcharts from 'highcharts';
+
+export default {
+  mounted(){
+    Highcharts.chart('container', {
+      chart : {
+        type : 'bar'
+      },
+      title : {
+        text : 'Fruit Consumption'
+      },
+      xAxis : {
+        categories: ['apple', 'Banana', 'Orange']
+      },
+      yAxis: {
+        title : {
+          text : 'Fruit eaten'  
+        }
+      },
+      series: [{
+        name: 'Jane',
+        data: [1,0,4]
+      }, {
+        name: 'John',
+        data: [5,7,3]
+      }]
+    });
+  }
+}
+</script>
