d085823d2b7e09c0104a41ae173fc3e5fdf94746
soft91 <soft4750@gmail.com>
2020-07-09 22:24:41 +0900

    Vue Component Props Test

---
+++ vue-component-props-ex/props-example/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="헬로우 월드 컴포넌트에 데이터 통신"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
