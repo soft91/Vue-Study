ff3e067ef19415173fbbcf71ff1ec22ac55c29d7
soft91 <soft4750@gmail.com>
2020-09-22 15:56:22 +0900

    v-for 예제

---
+++ vue-for-ex/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <ul v-for="item in 5" :key ="item.id">
+      <li>Item의 값 : {{ item }} </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;  
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+</style>
